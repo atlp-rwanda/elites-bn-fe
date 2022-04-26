dc875ae22678928f4abaacb4120d805beee31bfc
Honore IRADUKUNDA <ihonore01@gmail.com>
2022-04-26 16:24:45 +0200

    This is a combination of 6 commits.
    created empty react project structure
    
    modified README
    
    added PR template
    
    added directories
    
    configured redux
    
    configured mui theme, and added react-router

---
+++ src/App.js
@@ -0,0 +1,28 @@
+/* eslint-disable jsx-quotes */
+import React from 'react';
+import { ThemeProvider } from '@emotion/react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import './App.css';
+import Accommodations from './components/accommodations/Accommodations';
+import Home from './components/home/Home';
+import theme from './theme';
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <BrowserRouter>
+          <header>
+            <Link to='/'>HOME</Link>
+          </header>
+          <Routes>
+            <Route path='/accommodations' element={<Accommodations />} />
+            <Route path='/' element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
