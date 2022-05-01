7cc5d2b1c52b78d321e010ecb9eb0d33667a01ba
gihozoinnocente <igihozoinnocente@gmail.com>
2022-05-01 12:33:39 +0200

    fixing snapshot test

--- test/Home.test.js
+++ test/Home.test.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import '@testing-library/jest-dom';
-import App from './App';
 import Home from '../src/components/home/Home';
 
 describe('Snapshot Test', () => {
-  test('Snapshot for App Component', () => {
+  test('Snapshot for Home Component', () => {
     const renderedComponent = renderer.create(<Home />).toJSON();
     expect(renderedComponent).toMatchSnapshot();
   });
