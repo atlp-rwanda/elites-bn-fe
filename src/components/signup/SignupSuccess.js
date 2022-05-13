56398a6c30ec225d06197a4dbaeb9bbf68315f53
Niyongabo Aristide <niyongaboaristide17@gmail.com>
2022-05-13 14:09:04 +0200

    added fix on register success

--- src/components/signup/SignupSuccess.js
+++ src/components/signup/SignupSuccess.js
@@ -10,20 +10,20 @@ function SignupSuccess() {
 				</Grid>
 				<Grid item xs={12} md={6} lg={4} style={{ height: '100vh', background: '#07539F' }}>
 					<div style={{ marginTop: '40%' }}>
-						<Typography variant="h4" component="h3" style={{ color: '#f1f1f1', fontFamily: 'Montserrat, sans-serif', fontWeight:'bold' }}>
+						<Typography variant="h4" component="h3" style={{ color: '#f1f1f1', fontFamily: 'Montserrat, sans-serif', fontWeight:'bold', textAlign: 'center' }}>
 							Thank you for
 						</Typography>
-						<Typography variant="h4" component="h3" style={{ color: '#f1f1f1', fontFamily: 'Montserrat, sans-serif', fontWeight:'bold' }}>
+						<Typography variant="h4" component="h3" style={{ color: '#f1f1f1', fontFamily: 'Montserrat, sans-serif', fontWeight:'bold', textAlign: 'center' }}>
 							Registering with us!
 						</Typography>
 						<br />
-						<Typography variant="body1" component="h3" style={{ color: '#f1f1f1', fontFamily: 'Montserrat, sans-serif' }}>
+						<Typography variant="body1" component="h3" style={{ color: '#f1f1f1', fontFamily: 'Montserrat, sans-serif', textAlign: 'center' }}>
 							To continue to the application, please verify your email and login!
 						</Typography>
 						<br />
 						<br />
 						<br />
-						<Button component={Link} to="/signin" variant="outlined" style={{ color: '#f1f1f1', minWidth: '40%', borderColor: '#f1f1f1', borderRadius: '20px' }}>SIGN IN</Button>
+						<p style={{textAlign: 'center',}}><Button component={Link} to="/signin" variant="outlined" style={{ color: '#f1f1f1', minWidth: '40%', borderColor: '#f1f1f1', borderRadius: '20px' }}>SIGN IN</Button></p>
 					</div>
 				</Grid>
 				<Grid item md={3} lg={4}>
