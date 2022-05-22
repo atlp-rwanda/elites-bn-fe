932d5c7b0abe4c095619bad7bf081186c400e13b
Honore IRADUKUNDA <ihonore01@gmail.com>
2022-05-22 17:35:04 +0200

    added reducers tests

--- test/actions/currentUserActions.test.js
+++ test/actions/currentUserActions.test.js
@@ -1,9 +1,14 @@
 import 'babel-polyfill';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
-import { SET_CURRENT_USER } from '../../src/redux/types';
+import {
+  SET_CURRENT_USER,
+  SET_CURRENT_USER_PROFILE,
+} from '../../src/redux/types';
 import { currentUserInfo } from '../dummyData';
-import setCurrentUser from '../../src/redux/actions/currentUserActions';
+import setCurrentUser, {
+  setCurrentUserProfile,
+} from '../../src/redux/actions/currentUserActions';
 
 const middleware = [thunk];
 const mockStore = configureStore(middleware);
@@ -15,6 +20,14 @@ describe('CURRENT USER', () => {
     store.dispatch(setCurrentUser(currentUserInfo));
     const expectedAction = [
       { type: SET_CURRENT_USER, payload: currentUserInfo },
+    ];
+    expect(store.getActions()).toEqual(expectedAction);
+  });
+  it('should dispatch SET_CURRENT_USER_PROFILE action', () => {
+    store.clearActions();
+    store.dispatch(setCurrentUserProfile(currentUserInfo));
+    const expectedAction = [
+      { type: SET_CURRENT_USER_PROFILE, payload: currentUserInfo },
     ];
     expect(store.getActions()).toEqual(expectedAction);
   });
